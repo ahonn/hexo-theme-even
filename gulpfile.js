f2c2217dd22d1100d4f442b571441b733a00c784
ahonn <ahonn95@outlook.com>
2016-08-25 23:33:41 +0800

    update gulpfile and add javascript folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,28 @@ var gulp     = require('gulp');
 var sass     = require('gulp-sass');
 var mincss   = require('gulp-clean-css');
 var prefixer = require('gulp-autoprefixer');
+var uglify   = require('gulp-uglify');
 var rimraf   = require('rimraf');
 
-gulp.task('clean', function (done) {
-  rimraf('./source/css/style.css', done);
-});
-
-// Compile Sass into CSS
-gulp.task('sass', function () {
-  gulp.src('./style/style.scss')
+gulp.task('stylesheet', function () {
+  gulp.src('./stylesheet/style.scss')
       .pipe(sass())
       .pipe(prefixer('last 3 versions'))
       .pipe(mincss())
       .pipe(gulp.dest('./source/css'));
 });
 
-// Watch for changes to Sass
-gulp.task('watch', function () {
-  gulp.watch('./style/**/*.scss', ['sass']);
+gulp.task('javascript', function () {
+  gulp.src('./javascript/theme.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('./source/js'));
 });
 
-gulp.task('build', ['clean', 'sass']);
+gulp.task('watch', function () {
+  gulp.watch('./stylesheet/**/*.scss', ['stylesheet']);
+  gulp.watch('./javascript/*.js', ['javascript']);
+});
+
+gulp.task('build', ['stylesheet', 'javascript']);
 
 gulp.task('default', ['build', 'watch']);
