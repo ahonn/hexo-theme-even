2b94f41585d09c3da413e737aaea1b7ae813494c
ahonn <ahonn95@outlook.com>
2016-11-28 23:08:15 +0800

    change paginator text when mouse hover

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,88 +1,113 @@
-"use strict";
+"use strict"
 
 $(function() {
   // Back to top
   (function () {
-    var $back2top = $("#back2top");
+    var $back2top = $("#back2top")
 
     $(window).scroll(function () {
       if($(window).scrollTop() > 100) {
         $back2top.animate({
           opacity: 1
-        }, 'slow', 'ease');
+        }, "slow", "ease")
       } else {
         $back2top.animate({
           opacity: 0
-        }, 'slow', 'ease');
+        }, "slow", "ease")
       }
-    });
+    })
 
     function smoothScroll(el, to, duration) {
       if (duration < 0) {
-        return;
+        return
       }
-      var difference = to - $(window).scrollTop();
-      var perTick = difference / duration * 10;
+      var difference = to - $(window).scrollTop()
+      var perTick = difference / duration * 10
       setTimeout(function() {
         if (!isNaN(parseInt(perTick, 10))) {
-          window.scrollTo(0, $(window).scrollTop() + perTick);
-          smoothScroll(el, to, duration - 10);
+          window.scrollTo(0, $(window).scrollTop() + perTick)
+          smoothScroll(el, to, duration - 10)
         }
-      }.bind(this), 10);
+      }.bind(this), 10)
     }
 
     $back2top.click(function (e) {
-      e.preventDefault();
-      smoothScroll($(window), 0, 200);
-    });
-  })();
+      e.preventDefault()
+      smoothScroll($(window), 0, 200)
+    })
+  })()
 
   // Post follow sidebar
-  (function() {
+  ;(function() {
     var $sidebar    = $("#sidebar"),
-        $tags       = $('.tags'),
+        $tags       = $(".tags"),
         $headerlink = $(".headerlink"),
-        $toclink    = $(".toc-link");
+        $toclink    = $(".toc-link")
 
     if ($sidebar.length) {
       var minScrollTop = $sidebar.offset().top - 15,
-          maxScrollTop = $tags.offset().top - $sidebar.height() - 15;
+          maxScrollTop = $tags.offset().top - $sidebar.height() - 15
 
       $(window).load(function() {
-        maxScrollTop = $tags.offset().top - $sidebar.height() - 15;
-      });
+        maxScrollTop = $tags.offset().top - $sidebar.height() - 15
+      })
       $(window).scroll(function () {
-        var scrollTop     = $(window).scrollTop();
+        var scrollTop     = $(window).scrollTop()
 
         if (minScrollTop < scrollTop && scrollTop < maxScrollTop) {
           $sidebar.css({
             "position": "fixed",
             "top": "15px"
-          });
+          })
         } else if (scrollTop > maxScrollTop) {
           $sidebar.css({
             "position": "absolute",
             "top": maxScrollTop + 15
-          });
+          })
         } else {
           $sidebar.css({
             "position": "absolute",
             "top": minScrollTop + 15
-          });
+          })
         }
 
-        for(var i = 0; i < $headerlink.length; i++) {
+        for(var i = 0 ;i < $headerlink.length; i++) {
           var eqLength = i + 1 === $headerlink.length,
               minTop = $($headerlink[i]).offset().top - 20,
-              maxTop = eqLength ? Infinity : $($headerlink[i+1]).offset().top - 20;
+              maxTop = eqLength ? Infinity : $($headerlink[i+1]).offset().top - 20
 
           if (minTop < scrollTop && scrollTop <= maxTop) {
-            $($toclink[i]).addClass("active");
+            $($toclink[i]).addClass("active")
           } else {
-            $($toclink[i]).removeClass("active");
+            $($toclink[i]).removeClass("active")
           }
         }
-      });
+      })
     }
-  })();
-});
+  })()
+
+  ;(function () {
+    function hoverPaginator(el, elt, text) {
+      var beforeText = elt.text() 
+      el
+        .on("mouseover", function () {
+          elt.text(text)
+          elt.css({ "font-size": "18px" })
+        })
+        .on("mouseout", function () {
+          elt.text(beforeText)
+          elt.css({ "font-size": "20px" })
+        })
+    }
+
+    var $prev = $("#prev"),
+        prevText = $prev.find('span'),
+        prevTitle = $prev[0].dataset.title
+    hoverPaginator($prev, prevText, prevTitle)
+
+    var $next = $("#next"),
+        nextText = $next.find('span'),
+        nextTitle = $next[0].dataset.title;
+    hoverPaginator($next, nextText, nextTitle)
+  })()
+})
